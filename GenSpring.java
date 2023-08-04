83db8d4ad30b96017a4df59013b70754782bc025
pmj-mcp <139746963+pmj-mcp@users.noreply.github.com>
2023-08-04 11:06:22 +0200

    correction GenSpring

--- GenSpring.java
+++ GenSpring.java
@@ -7,6 +7,10 @@ import java.util.*;
  */
 class GenSpring {
 
+	public static String camelCase(String str){
+		return str.toLowerCase().charAt(0)+str.substring(1);
+	}
+
 	public static void mkdir(String path){
     	try{
 			Path p = Paths.get(path); 
@@ -44,32 +48,57 @@ class GenSpring {
 	    }
     }
 
-
+	enum Keys{
+		PACKAGE,
+		DAL,
+		ENTITIES,
+		REPOSITORIES,
+		SERVICES,
+		CONTROLLERS,
+		MODELS,
+		ENTITY,
+		ENTITY_LOWER,
+		ENTITY_CAMEL,
+		LENGTH_100,
+		TARGET_DIR
+		;
+	}
 	public static void main(String args[]){
+
+
+
+		if (args.length<2){
+			System.err.println("Usage:");
+			System.err.println("  java GenSpring com.example.myproject [../ExistsingTargetDir]");
+			System.exit(0);
+		}
 		String pkg = args[0];
 		String entity = args[1];
-		Map entities = Map.of(
-			"package", pkg,
-			"dal", "dal",
-			"repositories", "repositories",
-			"services", "services",
-			"controllers", "controllers",
-			"models", "models",
-			"entities", "entities",
-			"Entity", entity,
-			"entity", entity.toLowerCase(),
-			"100", "?"
-	   	);
+		String targetdir = args.length>2 ? args[2] : "./";
+		Map entities = new HashMap(Map.of(
+			Keys.PACKAGE, pkg,
+			Keys.DAL, "dal",
+			Keys.REPOSITORIES, "repositories",
+			Keys.SERVICES, "services",
+			Keys.CONTROLLERS, "controllers",
+			Keys.MODELS, "models",
+			Keys.ENTITIES, "entities",
+			Keys.ENTITY, entity,
+			Keys.ENTITY_LOWER, entity.toLowerCase(),
+			Keys.LENGTH_100, "100"
+	   	));
+		entities.put(Keys.TARGET_DIR, targetdir);
+		entities.put(Keys.ENTITY_CAMEL, camelCase(entity));
 
 
 		//------------------------------------------------------------------------------------
 
 		System.out.printf("Entity : %s Creating...\n",entity);
-	   	mkdir(String.format("./%s",entities.get("dal")));
-	   	mkdir(String.format("./%s/%s",entities.get("dal"),entities.get("entities")));
+	   	mkdir(String.format("%s/%s",entities.get(Keys.TARGET_DIR),entities.get(Keys.DAL)));
+	   	mkdir(String.format("%s/%s/%s",entities.get(Keys.TARGET_DIR),entities.get(Keys.DAL),entities.get(Keys.ENTITIES)));
 		//------------------------------------------------------------------------------------
 	   	writeFile(
-	   		String.format("./%s/%s/%sEntity.java", entities.get("dal"),entities.get("entities"),entities.get("Entity")),
+	   		String.format("%s/%s/%s/%sEntity.java",entities.get(Keys.TARGET_DIR), entities.get(Keys.DAL),entities.get(Keys.ENTITIES),entities.get(Keys.ENTITY)),
 	   		String.format("""
 package %s.%s.%s; 
 
@@ -81,13 +110,13 @@ import lombok.Setter;
 @Table(name = \"%s\")	// Nom de la table en DB
 @Getter
 @Setter
-Class %s {
+public class %sEntity {
 	@Id		// Primary Key
-	@GeneratedValue(strategy = GeneratedValue.IDENTITY)	//Auto-Incrémentée
+	@GeneratedValue(strategy = GenerationType.IDENTITY)	//Auto-Incrémentée
 	private Integer id;
 	
 	@Column (
-		Length = %s/*,
+		length = %s/*,
 		name = "name",	//Permet de spécifier la nom du champ en DB (par def : nom du membre lowercase()-
 		unique = true,	//Rend le champs unique en DB (par def: false)
 		nullable = true	//Rend le champ nullable zn DB (par def: false)
@@ -95,21 +124,21 @@ Class %s {
 	)
 	private String name;
 }""",
-				entities.get("package"),
-				entities.get("dal"),
-				entities.get("entities"),
-				entities.get("Entity"),
-				entities.get("Entity"),
-				entities.get("entity"),
-				entities.get("100")
+				entities.get(Keys.PACKAGE),
+				entities.get(Keys.DAL),
+				entities.get(Keys.ENTITIES),
+				entities.get(Keys.ENTITY),
+				entities.get(Keys.ENTITY_LOWER),
+				entities.get(Keys.ENTITY),
+				entities.get(Keys.LENGTH_100)
 			)
 		);
 
 		//------------------------------------------------------------------------------------
-	   	mkdir(String.format("./%s/%s",entities.get("dal"),entities.get("repositories")));
+	   	mkdir(String.format("%s/%s/%s",entities.get(Keys.TARGET_DIR),entities.get(Keys.DAL),entities.get(Keys.REPOSITORIES)));
 		//------------------------------------------------------------------------------------
 	   	writeFile(
-	   		String.format("./%s/%s/%sRepository.java", entities.get("dal"),entities.get("repositories"),entities.get("Entity")),
+	   		String.format("%s/%s/%s/%sRepository.java",entities.get(Keys.TARGET_DIR), entities.get(Keys.DAL),entities.get(Keys.REPOSITORIES),entities.get(Keys.ENTITY)),
 	   		String.format("""
 package %s.%s.%s; 
 
@@ -120,34 +149,30 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface %sRepository extends JpaRepository<%sEntity, Integer> {
 }""",
-				entities.get("package"),
-				entities.get("dal"),
-				entities.get("repositories"),
-				entities.get("package"),
-				entities.get("dal"),
-				entities.get("entities"),
-				entities.get("Entity"),
-				entities.get("Entity"),
-				entities.get("Entity"),
-				entities.get("entity"),
-				entities.get("Entity"),
-				entities.get("Entity"),
-				entities.get("Entity")
+				entities.get(Keys.PACKAGE),
+				entities.get(Keys.DAL),
+				entities.get(Keys.REPOSITORIES),
+				entities.get(Keys.PACKAGE),	//Import
+				entities.get(Keys.DAL),
+				entities.get(Keys.ENTITIES),
+				entities.get(Keys.ENTITY),
+				entities.get(Keys.ENTITY),		//interface ...Repository
+				entities.get(Keys.ENTITY)		//<...Entity,
 			)
 		);
 
 		//------------------------------------------------------------------------------------
-	   	mkdir(String.format("./%s",entities.get("services")));
+	   	mkdir(String.format("%s/%s",entities.get(Keys.TARGET_DIR),entities.get(Keys.SERVICES)));
 		//------------------------------------------------------------------------------------
 	   	writeFile(
-	   		String.format("./%s/%sService.java", entities.get("services"),entities.get("Entity")),
+	   		String.format("%s/%s/%sService.java",entities.get(Keys.TARGET_DIR), entities.get(Keys.SERVICES),entities.get(Keys.ENTITY)),
 	   		String.format("""
 package %s.%s;
 
 import %s.%s.%s.%sEntity;
 
 import java.util.Optional;
-import java.util.Colletction;
+import java.util.Collection;
 
 public interface %sService {
     Collection<%sEntity> findAll();
@@ -155,22 +180,22 @@ public interface %sService {
 
     void insert(%sEntity entity);
 }""",
-				entities.get("package"), //Pkg
-				entities.get("services"),
-				entities.get("package"), //Import
-				entities.get("dal"),
-				entities.get("entities"),
-				entities.get("Entity"), 
-				entities.get("Entity"),	//Interface
-				entities.get("Entity"),	//findAll
-				entities.get("entity"),	//findonebyid
-				entities.get("Entity")	//Insert
+				entities.get(Keys.PACKAGE), //Pkg
+				entities.get(Keys.SERVICES),
+				entities.get(Keys.PACKAGE), //Import
+				entities.get(Keys.DAL),
+				entities.get(Keys.ENTITIES),
+				entities.get(Keys.ENTITY),
+				entities.get(Keys.ENTITY),	//Interface
+				entities.get(Keys.ENTITY),	//findAll
+				entities.get(Keys.ENTITY),	//findonebyid
+				entities.get(Keys.ENTITY)	//Insert
 			)
 		);
 
 		//------------------------------------------------------------------------------------
 	   	writeFile(
-	   		String.format("./%s/%sServiceImpl.java", entities.get("services"),entities.get("Entity")),
+	   		String.format("%s/%s/%sServiceImpl.java",entities.get(Keys.TARGET_DIR), entities.get(Keys.SERVICES),entities.get(Keys.ENTITY)),
 	   		String.format("""
 package %s.%s;
 
@@ -180,7 +205,7 @@ import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
-import java.util.Colletction;
+import java.util.Collection;
 
 @Service
 public class %sServiceImpl implements %sService {
@@ -192,7 +217,7 @@ public class %sServiceImpl implements %sService {
     }
 
     public Collection<%sEntity> findAll() {
-        return this.%sRepository.findAll());
+        return this.%sRepository.findAll();
     }
 
     public Optional<%sEntity> findOneById(int id) {
@@ -203,45 +228,48 @@ public class %sServiceImpl implements %sService {
         this.%sRepository.save(entity);
     }
 }""",
-				entities.get("package"), //Pkg
-				entities.get("services"),
-				entities.get("package"), //Import Entity
-				entities.get("dal"),
-				entities.get("entities"),
-				entities.get("Entity"), 
-				entities.get("package"), //Import Repository
-				entities.get("dal"),
-				entities.get("entities"),
-				entities.get("Entity"), 
-				entities.get("Entity"),	//Class
-				entities.get("Entity"),	//Impl
-				entities.get("Entity"),	//Repository
-				entities.get("entity"),	
-				entities.get("Entity"),	//ctor
-				entities.get("Entity"),	
-				entities.get("entity"),	
-				entities.get("entity"),	//init
-				entities.get("entity"),	
-				entities.get("Entity"),	//findAll
-				entities.get("entity"),	//repository.findAll
-				entities.get("Entity"),	//findonebyid
-				entities.get("entity"),	//repository.findonebyid
-				entities.get("Entity"),	//Insert
-				entities.get("entity")	//repository.save
+				entities.get(Keys.PACKAGE), //Pkg
+				entities.get(Keys.SERVICES),
+				entities.get(Keys.PACKAGE), //Import Entity
+				entities.get(Keys.DAL),
+				entities.get(Keys.ENTITIES),
+				entities.get(Keys.ENTITY),
+				entities.get(Keys.PACKAGE), //Import Repository
+				entities.get(Keys.DAL),
+				entities.get(Keys.REPOSITORIES),
+				entities.get(Keys.ENTITY),
+				entities.get(Keys.ENTITY),	//Class
+				entities.get(Keys.ENTITY),	//Impl
+				entities.get(Keys.ENTITY),	//Repository
+				entities.get(Keys.ENTITY_CAMEL),
+				entities.get(Keys.ENTITY),	//ctor
+				entities.get(Keys.ENTITY),
+				entities.get(Keys.ENTITY_CAMEL),
+				entities.get(Keys.ENTITY_CAMEL),	//init
+				entities.get(Keys.ENTITY_CAMEL),
+				entities.get(Keys.ENTITY),	//findAll
+				entities.get(Keys.ENTITY_CAMEL),	//repository.findAll
+				entities.get(Keys.ENTITY),	//findonebyid
+				entities.get(Keys.ENTITY_CAMEL),	//repository.findonebyid
+				entities.get(Keys.ENTITY),	//Insert
+				entities.get(Keys.ENTITY_CAMEL)	//repository.save
 			)
 		);
 
 
 		//------------------------------------------------------------------------------------
-		mkdir(String.format("./%s",entities.get("controllers")));
-		mkdir(String.format("./%s/%s",entities.get("controllers"),entities.get("models")));
+		mkdir(String.format("%s/%s",entities.get(Keys.TARGET_DIR),entities.get(Keys.CONTROLLERS)));
+		mkdir(String.format("%s/%s/%s",entities.get(Keys.TARGET_DIR),entities.get(Keys.CONTROLLERS),entities.get(Keys.MODELS)));
 		//------------------------------------------------------------------------------------
 		writeFile(
-			String.format("./%s/%s/%s.java", entities.get("controllers"),entities.get("models"),entities.get("Entity")),
+			String.format("%s/%s/%s/%s.java",entities.get(Keys.TARGET_DIR), entities.get(Keys.CONTROLLERS),entities.get(Keys.MODELS),entities.get(Keys.ENTITY)),
 			String.format("""
 package %s.%s.%s;
 
 import %s.%s.%s.%sEntity;
+
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.Length;
 import lombok.Builder;
 import lombok.Setter;
 import lombok.Getter;
@@ -260,7 +288,7 @@ public class %s {
     public static %s fromEntity(%sEntity entity) {
         %s.%sBuilder builder = new %s.%sBuilder()
                 .id(entity.getId())
-                .imma(entity.getName());
+                .name(entity.getName());
 
         return builder.build();
     }
@@ -271,31 +299,31 @@ public class %s {
         return entity;
     }
 }""",
-				entities.get("package"),	//Pkg
-				entities.get("controllers"),
-				entities.get("models"),
-				entities.get("package"), //Import Entity
-				entities.get("dal"),
-				entities.get("entities"),
-				entities.get("Entity"), 
-				entities.get("Entity"),	//Class ...
-				entities.get("100"),	//Length(Max=...
-				entities.get("Entity"),	//... fromEntity(
-				entities.get("Entity"),	//...Entity
-				entities.get("Entity"),	//...Builder
-				entities.get("Entity"),
-				entities.get("Entity"),
-				entities.get("Entity"),
-				entities.get("Entity"),	//...Entity toEntity(
-				entities.get("Entity"),	//...Entity
-				entities.get("Entity")	//...Entity
+				entities.get(Keys.PACKAGE),	//Pkg
+				entities.get(Keys.CONTROLLERS),
+				entities.get(Keys.MODELS),
+				entities.get(Keys.PACKAGE), //Import Entity
+				entities.get(Keys.DAL),
+				entities.get(Keys.ENTITIES),
+				entities.get(Keys.ENTITY),
+				entities.get(Keys.ENTITY),	//Class ...
+				entities.get(Keys.LENGTH_100),	//Length(Max=...
+				entities.get(Keys.ENTITY),	//... fromEntity(
+				entities.get(Keys.ENTITY),	//...Entity
+				entities.get(Keys.ENTITY),	//...Builder
+				entities.get(Keys.ENTITY),
+				entities.get(Keys.ENTITY),
+				entities.get(Keys.ENTITY),
+				entities.get(Keys.ENTITY),	//...Entity toEntity(
+				entities.get(Keys.ENTITY),	//...Entity
+				entities.get(Keys.ENTITY)	//...Entity
 			)
 		);
 		//------------------------------------------------------------------------------------
 		writeFile(
-				String.format("./%s/%sController.java", entities.get("controllers"),entities.get("Entity")),
+				String.format("%s/%s/%sController.java",entities.get(Keys.TARGET_DIR), entities.get(Keys.CONTROLLERS),entities.get(Keys.ENTITY)),
 				String.format("""
-package %s.%scontrollers;
+package %s.%s;
 
 import %s.%s.%sService;
 import %s.%s.%s.%sEntity;
@@ -306,8 +334,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.annotation.Secured;
-import org.springframework.security.access.prepost.PreAuthorize;
+//import org.springframework.security.access.annotation.Secured;
+//import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
@@ -358,50 +386,47 @@ public class %sController {
     }
 
 }""",
-				entities.get("package"), //Pkg
-				entities.get("controllers"),
-				entities.get("package"), //Import Service
-				entities.get("services"),
-				entities.get("Entity"),
-				entities.get("package"), //Import Entity
-				entities.get("dal"),
-				entities.get("entities"),
-				entities.get("Entity"), 
-				entities.get("package"), //Import Model
-				entities.get("controllers"),
-				entities.get("models"),
-				entities.get("Entity"), 
-				entities.get("Entity"), //RequestMapping
-				entities.get("Entity"),	//Class
-				entities.get("Entity"),	//var Service
-				entities.get("entity"),	
-				entities.get("Entity"),	//ctor
-				entities.get("Entity"),	
-				entities.get("entity"),	
-				entities.get("entity"),	
-				entities.get("entity"),	
-				entities.get("Entity"),	//getAllAction
-				entities.get("Entity"),	
-				entities.get("entity"),		//...service
-				entities.get("Entity"),		//List<...>
-				entities.get("entity"),		//...list
-				entities.get("Entity"),		//map(...
-				entities.get("entity"),		//return
-				entities.get("Entity"),	//getOneAction
-				entities.get("Entity"),		//...Entity
-				entities.get("entity"),		//...service
-				entities.get("Entity"),		//return
-				entities.get("Entity"),	//<...> post*Action
-				entities.get("Entity"),		//post...Action
-				entities.get("Entity"),		//Param
-				entities.get("Entity"),		//...Entity
-				entities.get("entity"),		//...service
-				entities.get("Entity")		//return
+				entities.get(Keys.PACKAGE), //Pkg
+				entities.get(Keys.CONTROLLERS),
+				entities.get(Keys.PACKAGE), //Import Service
+				entities.get(Keys.SERVICES),
+				entities.get(Keys.ENTITY),
+				entities.get(Keys.PACKAGE), //Import Entity
+				entities.get(Keys.DAL),
+				entities.get(Keys.ENTITIES),
+				entities.get(Keys.ENTITY),
+				entities.get(Keys.PACKAGE), //Import Model
+				entities.get(Keys.CONTROLLERS),
+				entities.get(Keys.MODELS),
+				entities.get(Keys.ENTITY),
+				entities.get(Keys.ENTITY), //RequestMapping
+				entities.get(Keys.ENTITY),	//Class
+				entities.get(Keys.ENTITY),	//var Service
+				entities.get(Keys.ENTITY_CAMEL),
+				entities.get(Keys.ENTITY),	//ctor
+				entities.get(Keys.ENTITY),
+				entities.get(Keys.ENTITY_CAMEL),
+				entities.get(Keys.ENTITY_CAMEL),
+				entities.get(Keys.ENTITY_CAMEL),
+				entities.get(Keys.ENTITY),	//getAllAction
+				entities.get(Keys.ENTITY),
+				entities.get(Keys.ENTITY_CAMEL),		//...service
+				entities.get(Keys.ENTITY),		//List<...>
+				entities.get(Keys.ENTITY_CAMEL),		//...list
+				entities.get(Keys.ENTITY),		//map(...
+				entities.get(Keys.ENTITY_CAMEL),		//return
+				entities.get(Keys.ENTITY),	//getOneAction
+				entities.get(Keys.ENTITY),		//...Entity
+				entities.get(Keys.ENTITY_CAMEL),		//...service
+				entities.get(Keys.ENTITY),		//return
+				entities.get(Keys.ENTITY),	//<...> post*Action
+				entities.get(Keys.ENTITY),		//post...Action
+				entities.get(Keys.ENTITY),		//Param
+				entities.get(Keys.ENTITY),		//...Entity
+				entities.get(Keys.ENTITY_CAMEL),		//...service
+				entities.get(Keys.ENTITY)		//return
 			)
 		);
-
-
-
 	}
 }
 
